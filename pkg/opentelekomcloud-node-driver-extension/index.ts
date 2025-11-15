a379b458181bd1fad3418576332df34d1693604b
anton-sidelnikov <a.sidelnikov@t-systems.com>
2025-11-15 13:26:18 +0100

    direct imports

--- pkg/opentelekomcloud-node-driver-extension/index.ts
+++ pkg/opentelekomcloud-node-driver-extension/index.ts
@@ -1,14 +1,11 @@
+// pkg/otc-node-driver-extension/index.ts
 import { importTypes } from '@rancher/auto-import';
-import { IPlugin } from '@shell/core/types';
+import CloudCredential from './cloud-credential/opentelekomcloud.vue';
+import MachineConfig   from './machine-config/opentelekomcloud.vue';
 
-// Init the package
-export default function(plugin: IPlugin): void {
-  // Auto-import model, detail, edit from the folders
+export default function(plugin: any) {
   importTypes(plugin);
 
-  // Provide plugin metadata from package.json
-  plugin.metadata = require('./package.json');
-
-  // Load a product
-  // plugin.addProduct(require('./product'));
+  plugin.register('cloud-credential', 'opentelekomcloud', CloudCredential);
+  plugin.register('machine-config',   'opentelekomcloud', MachineConfig);
 }
