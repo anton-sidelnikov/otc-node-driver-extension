ca4ae3f369add19a9f3708a4513b4fda3bad9eaf
anton-sidelnikov <a.sidelnikov@t-systems.com>
2025-11-15 14:13:15 +0100

    skip auto import

--- pkg/opentelekomcloud-node-driver-extension/index.ts
+++ pkg/opentelekomcloud-node-driver-extension/index.ts
@@ -1,11 +1,8 @@
 // pkg/otc-node-driver-extension/index.ts
-import { importTypes } from '@rancher/auto-import';
 import CloudCredential from './cloud-credential/opentelekomcloud.vue';
 import MachineConfig   from './machine-config/opentelekomcloud.vue';
 
 export default function(plugin: any) {
-  importTypes(plugin);
-
   plugin.register('cloud-credential', 'opentelekomcloud', CloudCredential);
   plugin.register('machine-config',   'opentelekomcloud', MachineConfig);
 }
