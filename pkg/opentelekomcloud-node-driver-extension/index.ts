9be3f8e8ad8dc22e807d878e4a77046ee25ffc15
anton-sidelnikov <a.sidelnikov@t-systems.com>
2025-11-25 13:36:17 +0100

    more logic

--- pkg/opentelekomcloud-node-driver-extension/index.ts
+++
@@ -1,8 +0,0 @@
-// pkg/otc-node-driver-extension/index.ts
-import CloudCredential from './cloud-credential/opentelekomcloud.vue';
-import MachineConfig   from './machine-config/opentelekomcloud.vue';
-
-export default function(plugin: any) {
-  plugin.register('cloud-credential', 'opentelekomcloud', CloudCredential);
-  plugin.register('machine-config',   'opentelekomcloud', MachineConfig);
-}
